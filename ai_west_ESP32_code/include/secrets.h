9e4658c302644663871ee1070f789ba972890549
Luka Alaverdashvili <luka.alaverda1@gmail.com>
2026-04-26 02:45:06 +0400

    finaly fixed all gitignores

--- ai_west_ESP32_code/include/secrets.h
+++
@@ -1,4 +0,0 @@
-#define WIFI_SSID "lukanini"
-#define WIFI_PASSWORD "niniluka101228"
-#define API_KEY "AIzaSyCVL4FiLNdErk65n_w_nCa6GzwOD94YAoo"
-#define DATABASE_URL "https://espproject-f7db2-default-rtdb.europe-west1.firebasedatabase.app/"
